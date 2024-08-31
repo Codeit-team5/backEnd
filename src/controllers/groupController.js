aa5883d65eb00151f57c48b18dbf5213b42a3fc9
kimbjabgok <144577911+kimbjabgok@users.noreply.github.com>
2024-08-31 14:40:45 +0900

    수정, 삭제 groupController.js

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -35,7 +35,7 @@ groupController.get('/api/groups',async(req,res)=>{
     else{
       console.log("잘못된 입력");
     }
-    
+
     const entireGroup = await groupService.show(keyword,isPublic);
     return res.json(entireGroup);
   }catch(error){
@@ -77,5 +77,62 @@ groupController.get('/api/groups/:groupId/is-public',async(req,res)=>{
   }
 })
 
+///////////group 수정
+groupController.patch('/api/groups/:groupId', async(req,res)=>{
+  try{
+    const id = req.params.groupId;                         //id와 수정된 그룹을 가져옴.
+    const fixedGroup = await groupService.fixGroup(id,req.body);
+
+    if(fixedGroup == "wrongResponse"){  //fixedGroup이 string wrongResponse라면 400
+      return res.status(400).json({message: "잘못된 요청입니다"})
+    }
+    else if(fixedGroup == "wrongPassword"){   //fixedGroup이 string wrongPassword라면 403
+      return res.status(403).json({message: "비밀번호가 틀렸습니다"})
+    }
+
+    return res.json(fixedGroup);
+  }catch(error) {
+    console.error("error!", error); // 나머지 에러는 error를 출력하며
+    res.status(404).json({message: "존재하지 않습니다"});//404 상태 출력
+  }
+})
+
+
+
+//group 삭제
+groupController.delete('/api/groups/:groupId',async(req,res)=>{
+  try{
+    const Id = req.params.groupId;
+    const password = req.body.password;
+    
+    //변수 선언해야함
+
+    //catch ~ try 오류
+    /*await groupService.deleteGroup(groupId, groupPassword);
+    return res.status(200).json({message : "그룹 삭제 성공"});
+    }
+    catch(error){
+    if(deleteGroup=="wrongError"){
+      return res.status(401).json({message : "비밀번호가 틀렸습니다"});
+    }else if (deleteGroup=="nonError"){
+      return res.status(400).json({message :'잘못된 요청입니다' });
+    }else{
+      console.error('error!',error);
+      return res.status(404).json({message :'존재하지 않습니다' });
+    }*/
+
+   const deletedGroup = await groupService.deleteGroup(Id,password);
+   if(deletedGroup == 'wrongError'){
+    return res.status(401).json({message : "비밀번호가 틀렸습니다"});
+   }else if (deletedGroup=="nonError"){
+    return res.status(400).json({message :'잘못된 요청입니다' });
+   }
+
+   return res.status(200).json({message : "그룹 삭제 성공"});
+  }catch(error) {
+    console.error("error!", error); // 나머지 에러는 error를 출력하며
+    res.status(404).json({message: "존재하지 않습니다"});//404 상태 출력
+  }
+});
 
 export default groupController;
