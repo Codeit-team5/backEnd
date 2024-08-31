847cda14fdf9da6358f1a370a11d975822110162
kimbjabgok <144577911+kimbjabgok@users.noreply.github.com>
2024-08-31 14:41:59 +0900

    수정 삭제 groupService.js

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -29,10 +29,55 @@ async function open(groupId){
   return await groupRepository.findByIsPublic(findGroup); 
 }
 
+//id와 객체를 받아서 그룹 수정
+async function fixGroup(groupId, newGroup){
+  const groupPassword = await groupRepository.findByPassword(groupId);
+
+  //양식을 입력하기 않았을 때
+  if(newGroup.name == null
+    || newGroup.password ==null //comment에서는 nickname이 없거나
+    || newGroup.imageUrl ==null
+    || newGroup.isPublic ==null
+    || newGroup.introduction ==null){//newComment의 content가 없거나 password가 없으면 잘못된 요청
+      return "wrongResponse";
+    }
+    //비밀 번호가 같지 않을 때
+  if(groupPassword.password !=newGroup.password){
+    return "wrongPassword";
+  }
+
+  return await groupRepository.fixByGroupId(groupId,newGroup);
+
+}
+
+
+//id를 받아서 입력과 같은 id라면 삭제.
+async function deleteGroup(groupId, groupPassword){
+    /*const deletedGroup = await groupRepository.deleteByGroupId(groupId, groupPassword);
+  
+    //어떤의미로 만들었는지
+    if (!deletedGroup){
+      return 'nonError';
+    }*/
+
+    //id가 맞는지 비교하려면 비밀 번호를 db에서 접근을 먼저 해야함.
+    const findPassword = await groupRepository.findByPassword(groupId);
+
+    console.log(findPassword.password);
+    //비밀 번호 맞는지 확인
+    if(findPassword.password!==groupPassword){   //controller에서 이미 password를 빼놨음
+      return 'wrongError';
+    }
+
+  return await groupRepository.deleteByGroupId(groupId);
+  }
+
 
 export default {
   register,
   show,
   compare,
-  open
+  open,
+  fixGroup,
+  deleteGroup
 };
