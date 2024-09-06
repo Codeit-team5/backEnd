c58b128b3670ec9f3595f7874491f5d66e5f7ce8
yonseicode <sangbindo@gmail.com>
2024-09-07 08:49:20 +0900

    cors 설정

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 /*dotenv 라이브러리를 사용하여 env 파일의 환경 변수들을 로드.
 express 웹 프레임워크를 불러와서 애플리케이션 서버를 구축할 준비.*/
@@ -8,6 +10,7 @@ import commentController from './controllers/commentController.js';
 import imageController from './controllers/imageController.js';
 
 const api = express();
+api.use(cors());
 api.use(express.json()); //client에게 JSON 데이터를 받을 때 JavaScript 객체로 변환
 api.use('',groupController); //나중에 /api 없애도 되는지 확인하기
 api.use('',postController); //나중에 /api 없애도 되는지 확인하기
