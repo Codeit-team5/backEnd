169f50ca7c1a7ab6343669539f03b9720bf0d484
kimbjabgok <144577911+kimbjabgok@users.noreply.github.com>
2024-09-07 08:22:34 +0900

    update imageController

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import imageService from '../services/imageService.js';
+import multer from 'multer';
 
 const imageController = express.Router();
 
+//따로 uploads 파일을 저장을 해 놓아야 함.
+//multer를 통해서 로컬에 uploads 파일에 이미지가 저장됨
+//db에 직접적으로 저장을 하려면 bytea로 속성 변경
+const upload = multer({ dest: 'uploads/'});
 
-//그룹 등록
-imageController.post('/api/image',async(req,res)=>{
-  
+
+imageController.post('/image', upload.single('image'), (req, res) => {
   try{
-    const newImage = await imageService.post(req.body);    //req의 내용을 받음
-    return res.json(newImage);
+    const filename = req.file.filename;
+    //경로를 서버에 전달
+    const path = `${filename}`;
+    res.status(200).json({ path });
   }
   catch (error) {
     console.error("error!", error); // 에러 로그에 실제 오류를 출력하도록 수정
@@ -17,4 +22,17 @@ imageController.post('/api/image',async(req,res)=>{
   }
 });
 
+//app.use('/profile', express.static('uploads/'));
+
 export default imageController;
+
+
+/*
+const formData = new FormData();
+formData.append('image', fileInput.files[0]);
+
+fetch('http://localhost:3000/image', {
+  method: 'POST',
+  body: formData
+});
+*/
